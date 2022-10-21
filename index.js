43ef505dce02a4a0e2461a12d5f5e2021c82f16e
Alfredo Ramirez <alfredo.ramirez@sysdig.com>
2022-10-21 16:59:26 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+class User {
+
+    constructor(){
+        this.message = 'Hello World';
+        console.log(this.message);
+    }
+    netUserName(){}
+    getUserStatus(){}
+}
+module.exports = User;
